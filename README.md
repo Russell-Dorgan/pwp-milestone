22eef56078a749c5eaf11da15848ea02aa2f147f
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-19 11:51:51 -0600

    created descriptions for a purpose, audience, goal, and clientele type. (phase-1)

---
+++ README.md
@@ -0,0 +1,1 @@
+creating pwp around vinyl sales
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><strong> Identifiers for a Successful Business: Purpose, Audience, Goal, Clientele Type </strong></title>
+</head>
+<p>
+
+	<h1><strong> Purpose </strong> </h1>
+
+		<ul>
+			<li>I would like my <strong> PWP </strong> to help me accomplish making sales and revenue through means of vinyl sales and other related merchandise.</li>
+		</ul>
+
+
+
+	<h2><strong> Audience </strong></h2>
+
+		<ul>
+			<li>Someone who would be viewing my site is an enthusiast if high-fidelity lossless audio.</li>
+			<li>My audience would be comprised of appreciators of good music pressed on quality vinyl.</li>
+		</ul>
+
+	<h3><strong> Goal </strong></h3>
+
+		<ul>
+			<li>A goal for my <strong> PWP </strong> would be to acquire part time self-employment and eventually bring in enough clients to fulfill full time self-employment.</li>
+			<li>Another goal would be to serve as a showcase of appreciation for the arts being sold.</li>
+		</ul>
+
+
+	<h4><strong> Persona </strong></h4>
+
+		<ul>
+			<li><strong>Name:</strong> John Doe </li>
+			<li><strong>Age:</strong> 32 </li>
+			<li><strong>Job:</strong> Office Work </li>
+			<li><strong>Relevant Technology:</strong> Mobile Devices and Laptop/Desktop</li>
+			<li><strong>Scenario they will use my site:</strong> Will use my site to purchase one of their favorite, latest underground albums that will get pressed up on 180 gram vinyl by our company. </li>
+		</ul>
+
+	<h5><strong> Frustrations that may be exhibited </strong></h5>
+		<ul>
+			<li>Since our company will be based in the USA if they live abroad IE Europe shipping will be about $27 vs $5 domestic so could cause some consumer complaints. </li>
+		</ul>
+
+	<h6><strong> Additional Info needed to achieve goal</strong></h6>
+		<ul>
+			<li>Since these are limited pressed vinyl with high demand they will go fast, to make sure you're able to catch the records right on release stay connected to our social medias (Instagram, Twitter) for release date and times in multiple time formats. </li>
+		</ul>
+
+</body>
+</html>
+
+
+

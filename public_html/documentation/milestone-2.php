f20c41d9f77d078265e7ae21e14245e533d76c6d
Russell Dorgan <clambakedbenz@gmail.com>
2020-04-14 08:33:41 -0600

    Added a wireframe and changed out phrase "page" to "section"

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,7 @@
 		<h1><strong> Planned Pages </strong></h1>
 
 			<ul>
-				<li>Home Page</li>
+				<li>Home</li>
 				<li>Records</li>
 				<li>Digital Releases</li>
 				<li>FAQ</li>
@@ -16,35 +16,35 @@
 			</ul>
 
 
-		<h2><strong> Home Page </strong></h2>
+		<h2><strong> Home </strong></h2>
 
 			<ul>
-				<li>Home Page will feature all of the releases we have put out so far in chronological order with a picture of the release, title, with a clickable link that will bring you to a dedicated page for that specific item</li>
+				<li>Home section will feature all of the releases we have put out so far in chronological order with a picture of the release, title, with a clickable link that will bring you to a dedicated page for that specific item</li>
 			</ul>
 
 
 		<h3><strong> Records </strong></h3>
 
 			<ul>
-				<li>The 'Records' page will be similar to the home page except will exclude any digital releases and only contain listings for physical, if item is sold out will drop to the bottom of the page but keep chronoligcal.</li>
+				<li>The 'Records' section will be similar to the home section except will exclude any digital releases and only contain listings for physical, if item is sold out will drop to the bottom of the page but keep chronoligcal.</li>
 			</ul>
 
 
 		<h4><strong> Digital Releases </strong></h4>
 
 			<ul>
-				<li>The 'Digital Releases' page will be similar to the home page except will exclude any physcal releases and only contain listings for digital.</li>
+				<li>The 'Digital Releases' section will be similar to the home section except will exclude any physcal releases and only contain listings for digital.</li>
 			</ul>
 
 
 		<h5><strong> FAQ </strong></h5>
 
 			<ul>
-				<li>The 'FAQ' page will contain information about where we are located, shipping information, return policies</li>
+				<li>The 'FAQ' section will contain information about where we are located, shipping information, return policies</li>
 			</ul>
 
 
 		<h6><strong> Contact </strong></h6>
 			<ul>
-				<li>The 'Contact' page will contain information on how to reach out to a company representative.</li>
+				<li>The 'Contact' section will contain information on how to reach out to a company representative.</li>
 			</ul>
